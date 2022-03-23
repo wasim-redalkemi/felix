a3590d505861b62086b76d58c5849539f5548588
wasim-redalkemi <89829722+wasim-redalkemi@users.noreply.github.com>
2022-03-23 12:09:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//change from github
 //css
 import "./css/style.scss";
 
